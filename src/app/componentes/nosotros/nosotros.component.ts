b3c8cbf41e26933e11144ff6c747c68c320200ef
Jeremy Sojos Fichamba <jeremy_sojos@hotmail.com>
2022-06-04 21:36:25 -0500

    asdadasddas

--- src/app/componentes/nosotros/nosotros.component.ts
+++ src/app/componentes/nosotros/nosotros.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NosotrosComponent implements OnInit {
 
+  serviciosList = [
+    {
+    title: "Tiroides",
+    desc: "Tratamiento de hipotiroidismo, hipertiroidismo, nódulos e incluso cáncer de tiroides. Además en nuestro instituto somos pioneros en la realización de biopsia de tiroides con citología in situ.",
+    image: "assets/backgrounds/card1.jpg"
+  },
+  {
+    title: "Tiroides",
+    desc: "Tratamiento de hipotiroidismo, hipertiroidismo, nódulos e incluso cáncer de tiroides. Además en nuestro instituto somos pioneros en la realización de biopsia de tiroides con citología in situ.",
+    image: "assets/backgrounds/card1.jpg",
+  },
+  {
+    title: "Tiroides",
+    desc: "Tratamiento de hipotiroidismo, hipertiroidismo, nódulos e incluso cáncer de tiroides. Además en nuestro instituto somos pioneros en la realización de biopsia de tiroides con citología in situ.",
+    image: "assets/backgrounds/card1.jpg",
+  },
+  {
+    title: "Tiroides",
+    desc: "Tratamiento de hipotiroidismo, hipertiroidismo, nódulos e incluso cáncer de tiroides. Además en nuestro instituto somos pioneros en la realización de biopsia de tiroides con citología in situ.",
+    image: "assets/backgrounds/card1.jpg",
+  },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {

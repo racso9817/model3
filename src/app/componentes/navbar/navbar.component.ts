302f2a17fc72ab8ca75abe2e04af58c2d2297ef3
Oscar Avila <oscaravilaa9817@gmail.com>
2022-06-04 23:35:35 -0500

    s

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -18,15 +18,23 @@ export class NavbarComponent implements OnInit {
 
   changeClass(){
     var nav = $(".navBar");
+    var logo = $(".logo");
+    var windowWidth = $(window).width();
     $(window).scroll(function() {
       if ($(window).scrollTop())  {
-        nav.addClass("sticky");
-        nav.removeClass("absolute")
+        nav.addClass("fixed-top");
         nav.css("background-color", "white")
+        logo.css("width","3vw");
+        if(windowWidth? windowWidth < 560: true){
+          logo.css("width","10vw");
+        }
       } else {
-        nav.removeClass("sticky");
-        nav.addClass("absolute")
+        nav.removeClass("fixed-top");
         nav.css("background-color", "transparent")
+        logo.css("width","5vw");
+        if(windowWidth? windowWidth < 560: true){
+          logo.css("width","20vw");
+        }
       }
     });  
   }
